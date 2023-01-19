3ec52b2982fa227b64184ae02495db03ced99183
admin <nileshsonawanen1@gmail.com>
2023-01-19 13:48:34 +0530

    ui improvement

--- app/src/main/java/com/nilscreation/photoeditor/MainActivity.java
+++ app/src/main/java/com/nilscreation/photoeditor/MainActivity.java
@@ -12,6 +12,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
@@ -20,9 +23,12 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     CardView btnGallery, btnCamera;
+    ImageSlider imageSlider;
     AdView mAdView;
 
     @Override
@@ -30,8 +36,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageSlider = findViewById(R.id.image_slider);
         btnGallery = findViewById(R.id.btnGallery);
         btnCamera = findViewById(R.id.btnCamera);
+
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.goggles, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.goggles, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.goggles, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
 
         MobileAds.initialize(MainActivity.this, new OnInitializationCompleteListener() {
             @Override
